ffb6cf67276fbd107d10a139d7f9ebf9bfa6a3a1
enrique7mc <enrique7mc@gmail.com>
2015-06-17 12:53:54 -0500

    Implements the functionality

--- MyApplication/app/src/main/java/com/enrique7mc/compassapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/enrique7mc/compassapp/MainActivity.java
@@ -1,17 +1,49 @@
 package com.enrique7mc.compassapp;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SensorEventListener{
+	@InjectView(R.id.degreesLabel) TextView mDegreesLabel;
+	@InjectView(R.id.compassImageView) ImageView mCompassImageView;
+	private SensorManager mSensorManager;
+	private float mCurrentDegree;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+		ButterKnife.inject(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+				SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this);
 	}
 
 	@Override
@@ -34,5 +66,22 @@ public class MainActivity extends ActionBarActivity {
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float degree = Math.round(event.values[0]);
+		mDegreesLabel.setText("Rotation: " + Float.toString(degree) + " degrees");
+		RotateAnimation animation = new RotateAnimation(mCurrentDegree,-degree,
+				Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+		animation.setDuration(120);
+		animation.setFillAfter(true);
+		mCompassImageView.startAnimation(animation);
+		mCurrentDegree = -degree;
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int i) {
+		// not used
 	}
 }
